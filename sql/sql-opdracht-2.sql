c2a1909d3073ceddc4f19de5f335218b4066b210
KimLamers <kiml_1194@hotmail.com>
2023-12-12 11:36:01 +0100

    SQL Opdracht 2 (#5)

---
+++ sql/sql-opdracht-2.sql
@@ -0,0 +1,8 @@
+-- Create a report that shows the ContactName, CompanyName, ContactTitle and Phone number from the customers table sorted by Phone
+
+SELECT  ContactName,
+        CompanyName,
+        ContactTitle,
+        Phone
+  FROM  customers
+ORDER BY Phone;
