4061f9b372245b850b526e39e431effa6a45a75d
KimLamers <kiml_1194@hotmail.com>
2023-12-12 12:36:52 +0100

    SQL Opdracht 8 (#11)

---
+++ sql/sql-opdracht-8.sql
@@ -0,0 +1,7 @@
+-- Create a report showing ProductName, UnitPrice, QuantityPerUnit of products that are out of stock.
+
+SELECT  ProductName,
+        UnitPrice,
+        QuantityPerUnit
+  FROM  products
+ WHERE UnitsInStock = 0;
