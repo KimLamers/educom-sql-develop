36ec603870cc220ef9ccbf9a82929bf2f87b0d2b
KimLamers <kiml_1194@hotmail.com>
2023-12-12 13:46:03 +0100

    SQL Opdracht 13 (#16)

---
+++ sql/sql-opdracht-13.sql
@@ -0,0 +1,8 @@
+-- Create a report that shows the City, CompanyName, ContactName of customers from cities starting with A or B.
+
+SELECT  City,
+        CompanyName,
+        ContactName
+  FROM  customers
+ WHERE  City LIKE 'A%'
+    OR  City LIKE 'B%';
