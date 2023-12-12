ec6511905f854934009ffebad243b7f1253e0087
KimLamers <kiml_1194@hotmail.com>
2023-12-12 11:24:57 +0100

    SQL Opdracht 1 (#4)

---
+++ sql/sql-opdracht-1.sql
@@ -0,0 +1,5 @@
+-- Create a report that shows the CategoryName and Description from the categories table sorted by CategoryName.
+
+SELECT CategoryName, Description
+FROM categories
+ORDER BY CategoryName;
