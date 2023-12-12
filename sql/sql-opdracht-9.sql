47b9799c509f5c680ba17045d1d7cc9cfd66b0e6
KimLamers <kiml_1194@hotmail.com>
2023-12-12 12:50:51 +0100

    SQL Opdracht 9 (#12)

---
+++ sql/sql-opdracht-9.sql
@@ -0,0 +1,7 @@
+-- Create a report showing all the ContactName, Address, City of all customers not from Germany, Mexico, Spain.
+
+SELECT  ContactName,
+        Address,
+        City
+  FROM  customers
+ WHERE  Country NOT IN ('Germany', 'Mexico', 'Spain');
